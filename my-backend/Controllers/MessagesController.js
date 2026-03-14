0ce7f85a1b62a0e16eb2005d7d4116cc7839c97d
MinaamAhmad <minaamahmad@gmail.com>
2026-03-14 10:27:11 -0700

    initialize messages instance in MessagesController

--- my-backend/Controllers/MessagesController.js
+++ my-backend/Controllers/MessagesController.js
@@ -1,5 +1,7 @@
 import { Server } from "socket.io";
 import cors from cors
+import Message from "../Models/Messages.js";
+
 
 
 const io = new Server(httpserver,{
@@ -11,5 +13,8 @@ io.on("connection",(socket)=>{
     console.log("Comms are on ",socket.id)
 })
 httpserver.listen(3000)
+
+
+const messages= new
 
 module.exports=io
