3bdad31248d4048d664407ce790f1197011c83ec
MinaamAhmad <minaamahmad@gmail.com>
2026-03-16 09:49:23 -0700

    cleaned code

--- my-backend/routes/BookingRoute.js
+++ my-backend/routes/BookingRoute.js
@@ -10,19 +10,19 @@ import { protect, authorizeRoles } from "../Middlewares/auth.js";
 
 const router = express.Router();
 
-// Create a new booking
+
 router.post("/", protect, authorizeRoles("Guest"), createBooking);
 
-// Get user's bookings
+
 router.get("/", protect, authorizeRoles("Guest"), getBookingsByUser);
 
-// Get hotel's bookings
+
 router.get("/hotel/:hotelId", protect, authorizeRoles("Owner"), getBookingsByHotel);
 
-// Approve a booking
+
 router.put("/:id/approve", protect, authorizeRoles("Owner"), approveBooking);
 
-// Cancel a booking
+
 router.delete("/:id", protect, cancelBooking);
 
 export default router;
